695d9ceca0f2c25613deaad384965b70a4321d02
ht-nam <hoangthanhnam10@gmail.com>
2021-09-15 15:01:33 +0700

    fix some bug of Frame2

--- Frame2.py
+++ Frame2.py
@@ -29,6 +29,67 @@ class Frame2:
             command=self.__SignOut,
         ).place(x=10, y=187)
 
+        imgbt = ImageTk.PhotoImage(file=os.getcwd() + r"\resource\q.png")
+        bt1 = Button(
+            self.__frame2,
+            text="Điểm danh",
+            font=("Arial", 20, "bold"),
+            width=283,
+            height=50,
+            image=imgbt,
+            compound="center",
+        )
+        bt1.image_names = imgbt
+        bt1.place(x=10, y=243)
+
+        bt2 = Button(
+            self.__frame2,
+            text="Thông tin chi tiết",
+            font=("Arial", 20, "bold"),
+            width=283,
+            height=50,
+            image=imgbt,
+            compound="center",
+        )
+        bt2.image_names = imgbt
+        bt2.place(x=10, y=315)
+
+        bt3 = Button(
+            self.__frame2,
+            text="Sửa thông tin",
+            font=("Arial", 20, "bold"),
+            width=283,
+            height=50,
+            image=imgbt,
+            compound="center",
+        )
+        bt3.image_names = imgbt
+        bt3.place(x=10, y=387)
+
+        bt4 = Button(
+            self.__frame2,
+            text="Chương trình đào tạo",
+            font=("Arial", 20, "bold"),
+            width=283,
+            height=50,
+            image=imgbt,
+            compound="center",
+        )
+        bt4.image_names = imgbt
+        bt4.place(x=10, y=459)
+
+        bt5 = Button(
+            self.__frame2,
+            text="Comming Soon",
+            font=("Arial", 20, "bold"),
+            width=283,
+            height=50,
+            image=imgbt,
+            compound="center",
+        )
+        bt5.image_names = imgbt
+        bt5.place(x=10, y=531)
+
     def pack(self):
         self.__frame2.pack(fill="both", expand=1)
 
