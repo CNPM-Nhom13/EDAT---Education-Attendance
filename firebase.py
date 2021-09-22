32029e00e10b8863456994ef0b54e8de87dc1a03
ht-nam <hoangthanhnam10@gmail.com>
2021-09-22 17:15:57 +0700

    test realtime database

--- firebase.py
+++ firebase.py
@@ -22,8 +22,20 @@ config_a = {
     "appId": "1:264425807306:web:e5a50cd02888d3e655c333",
 }
 
+configDatabase = {
+    "apiKey": "AIzaSyB_LH8lyCg4bdgUHZhsmeyNmUtOjoB22mA",
+    "authDomain": "qlsv-ea25d.firebaseapp.com",
+    "databaseURL": "https://qlsv-ea25d-default-rtdb.firebaseio.com/",
+    "projectId": "qlsv-ea25d",
+    "storageBucket": "qlsv-ea25d.appspot.com",
+    "messagingSenderId": "911368744543",
+    "appId": "1:911368744543:web:04dd657c6f99b2bf27f5ea",
+    "measurementId": "G-1BKR6BV3NM",
+}
+
 firebase = pyrebase.initialize_app(config_a)
 storage = firebase.storage()
+database = pyrebase.initialize_app(configDatabase).database()
 
 dtbLocalPath = dtbCloudPath = "database/database.db"
 tnLocalPath = tnCloudPath = "recognizer/trainingData.yml"
@@ -47,3 +59,19 @@ def uploadDtb():
 
 def uploadTrainingData():
     storage.child(tnCloudPath).put(tnLocalPath)
+
+
+# setNewRecord
+# database.child("1111").set({"Time": 5})
+
+# getRecord
+# e = database.child("1111").get()
+# print(dict(e.val())["Time"])
+
+
+# updateRecord
+# database.child("1111").update({"Time": 20})
+
+
+# delete "1111"
+# database.child("1111").remove()
