7e885ceae0967d7927ad041304b7e0b534cbe888
ht-nam <hoangthanhnam10@gmail.com>
2021-09-26 23:01:22 +0700

    Add filter for class status in Teacher's App and fix some bug

--- Teacher'sApp/ViewMyClass.py
+++ Teacher'sApp/ViewMyClass.py
@@ -1,3 +1,4 @@
+import time
 from tkinter import Tk, Frame, Label, Button, Entry, messagebox, ttk
 import sqlite3, firebase, threading
 
@@ -6,6 +7,9 @@ class MyFrame1:
     def __init__(self, master):
         self.__master = master
         self.__SvTime = tuple("0" for i in range(8))
+        self.__SVOn = tuple()
+        self.__SVOff = tuple()
+        self.__status = "All"
         self.__frame = Frame(self.__master, width=600, height=520)
         self.__connect = sqlite3.connect(r"database\database.db")
         cursor = self.__connect.execute("SELECT * FROM people")
@@ -40,6 +44,17 @@ class MyFrame1:
             command=self.__frame.forget,
         ).place(x=250, y=465)
 
+        thr1 = threading.Thread(name=("Thr1"), target=self.__getSvTime)
+        thr1.start()
+
+        options = ("All", "Online", "Offline")
+        self.__cbb = ttk.Combobox(
+            self.__frame, values=options, font=("Arial", 20), width=6, foreground="blue"
+        )
+        self.__cbb.current(0)
+        self.__cbb.place(x=475, y=0)
+        self.__getStatus()
+
     def __start(self):
         self.__sta.place_forget()
         self.__sto = Button(
@@ -72,15 +87,29 @@ class MyFrame1:
             self.__frame1, text="Danh sách sinh viên", font=("Arial", 20, "bold")
         ).grid(row=0, column=0, columnspan=4)
         self.__table()
+        cbb = self.__cbb.get()
 
-        for q in range(len(self.__lstsv)):
+        lst = self.__lstsv
+        if self.__cbb.get() == "Online":
+            lst = tuple(
+                self.__lstsv[i]
+                for i in range(len(self.__lstsv))
+                if int(self.__SvTime[i]) > 0
+            )
+        elif self.__cbb.get() == "Offline":
+            lst = tuple(
+                self.__lstsv[i]
+                for i in range(len(self.__lstsv))
+                if int(self.__SvTime[i]) == 0
+            )
+        for q in range(len(lst)):
             Label(self.__frame1, text=str(q + 1), font=("Arial", 15)).grid(
                 row=q + 2, column=0
             )
-            Label(self.__frame1, text=self.__lstsv[q][3], font=("Arial", 15)).grid(
+            Label(self.__frame1, text=lst[q][3], font=("Arial", 15)).grid(
                 row=q + 2, column=1
             )
-            Label(self.__frame1, text=self.__lstsv[q][2], font=("Arial", 15)).grid(
+            Label(self.__frame1, text=lst[q][2], font=("Arial", 15)).grid(
                 row=q + 2, column=2
             )
             Label(
@@ -88,26 +117,55 @@ class MyFrame1:
                 text=str(0),
                 font=("Arial", 15),
             ).grid(row=q + 2, column=3)
-        self.__frame1.after(3000, self.__updateTime)
+        self.__frame1.after(1000, lambda: self.__updateTime(cbb, len(lst)))
 
-    def __updateTime(self):
-        thr1 = threading.Thread(name=("Thr1"), target=self.__getSvTime)
-        thr1.start()
-        for q in range(len(self.__SvTime)):
+    def __updateTime(self, cbb, i):
+        self.__thr1 = threading.Thread(name=("Thr1"), target=self.__getSvTime)
+        self.__thr1.start()
+
+        tp = self.__SvTime
+        if self.__cbb.get() == "Online":
+            tp = self.__SVOn
+        elif self.__cbb.get() == "Offline":
+            tp = self.__SVOff
+        for q in range(len(tp)):
             Label(
                 self.__frame1,
-                text=str(self.__SvTime[q]),
+                text=str(tp[q]),
                 font=("Arial", 15),
             ).grid(row=q + 2, column=3)
-        self.__frame1.after(5000, self.__updateTime)
+        a = (
+            len(self.__SVOn)
+            if self.__cbb.get() == "Online"
+            else len(self.__SVOff)
+            if self.__cbb.get() == "Offline"
+            else len(self.__SvTime)
+        )
+        if cbb != self.__cbb.get() or i != a:
+            self.__frame1.place_forget()
+            self.__frame1 = Frame(self.__frame, width=600, height=420)
+            self.__frame1.place(x=0, y=60)
+            self.__frame1Config()
+        else:
+            self.__frame1.after(3000, lambda: self.__updateTime(cbb, i))
 
     def __getSvTime(self):
         self.__SvTime = tuple(
             firebase.getTime(self.__lstsv[i][0]) for i in range(len(self.__lstsv))
         )
+        self.__SVOn = tuple(i for i in self.__SvTime if int(i) > 0)
+        self.__SVOff = tuple(i for i in self.__SvTime if int(i) == 0)
 
     def __table(self):
-        for q in range(len(self.__lstsv) + 1):
+        temp = len(self.__lstsv)
+        if sum(set(map(int, self.__SvTime))) == 0 and self.__status != "All":
+            time.sleep(2)
+        if self.__status == "Online":
+            temp = len([i for i in self.__SvTime if int(i) > 0])
+        elif self.__status == "Offline":
+            temp = len([i for i in self.__SvTime if int(i) == 0])
+
+        for q in range(temp + 1):
             Label(
                 self.__frame1,
                 width=10,
@@ -143,6 +201,10 @@ class MyFrame1:
         Label(self.__frame1, text="Họ tên", font=("Arial", 15)).grid(row=1, column=2)
         Label(self.__frame1, text="Thời gian", font=("Arial", 15)).grid(row=1, column=3)
 
+    def __getStatus(self):
+        self.__status = self.__cbb.get()
+        self.__cbb.after(10, self.__getStatus)
+
     def forget(self):
         self.__frame.forget()
 
@@ -150,6 +212,6 @@ class MyFrame1:
         self.__frame.pack()
 
 
-"""root = Tk()
+root = Tk()
 mf = MyFrame1(root)
-root.mainloop()"""
+root.mainloop()
