0a724548b6ced9686eb739b059ebfc5efb48c149
ht-nam <hoangthanhnam10@gmail.com>
2021-10-12 17:37:50 +0700

    Build finished app

--- main.py
+++ main.py
@@ -2,14 +2,19 @@ import Frame1, Frame2, threading, firebase
 from tkinter import *
 
 
-thr1 = threading.Thread(name="Thread-1", target=firebase.dowloadDtb)
-thr2 = threading.Thread(name="Thread-2", target=firebase.dowloadTrainingData)
-thr1.start()
-thr2.start()
-root = Tk()
-mg = Frame1.Frame1(root)
-root.mainloop()
-firebase.uploadDtb()
-firebase.uploadTrainingData()
+def main():
+    thr1 = threading.Thread(name="Thread-1", target=firebase.dowloadDtb)
+    thr2 = threading.Thread(name="Thread-2", target=firebase.dowloadTrainingData)
+    thr1.start()
+    thr2.start()
+    root = Tk()
+    mg = Frame1.Frame1(root)
+    root.mainloop()
+    firebase.uploadDtb()
+    firebase.uploadTrainingData()
+
 
 ##Tạm tắt tải lên xuống file training vì tải vài lần là hết sạch cloud
+
+if __name__ == "__main__":
+    main()
